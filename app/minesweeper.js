cdeacd8e13640adff3644f95addd429c9e3b4cab
Luis Caparroz <lcaparroz.dev@gmail.com>
2019-04-06 20:26:15 -0300

    Rename generateBlankMap to generateRawField
    This commit renames the Minesweeper function `generateBlankMap` to
    `generateRawField` and changes its behavior. Now, the function returns a
    matrix (Seq of Seqs) where each element is its own coordinate (List) in
    the matrix.

--- app/minesweeper.js
+++ app/minesweeper.js
@@ -1,6 +1,9 @@
-const { Map, List, Repeat } = require('immutable');
+const { List, Range } = require('immutable');
 
-exports.generateBlankMap = function(height, width) {
-  return List([]).setSize(height)
-    .map(_ => List(Repeat(null, width)));
+exports.generateRawField = function(height, width) {
+  return Range(0, height).map(
+    x => Range(0, width).map(
+      y => List([x, y])
+    )
+  );
 };
--- test/minesweeper.js
+++ test/minesweeper.js
@@ -1,19 +1,21 @@
 const assert = require('assert');
-const { fromJS } = require('immutable');
+const { List, Seq } = require('immutable');
 const minesweeper = require ("../app/minesweeper");
 
 describe("Minesweeper", function() {
-  describe("generateBlankMap", function() {
-    it("generates a List of Lists with null values", function() {
-      const expected_map = fromJS(
-        [[null, null, null],
-          [null, null, null],
-          [null, null, null]]
+  describe("generateRawField", function() {
+    it("returns a matrix with elements as their own coordinates", function() {
+      const expected_field = Seq(
+        [
+          Seq([List([0, 0]), List([0, 1]), List([0, 2])]),
+          Seq([List([1, 0]), List([1, 1]), List([1, 2])]),
+          Seq([List([2, 0]), List([2, 1]), List([2, 2])])
+        ]
       );
 
-      const result_map = minesweeper.generateBlankMap(3, 3);
+      const result_field = minesweeper.generateRawField(3, 3);
 
-      assert(expected_map.equals(result_map));
+      assert(expected_field.equals(result_field));
     });
   });
 });
