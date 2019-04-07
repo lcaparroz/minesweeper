09921dd914de2cd7eea24b2aa2d2d57c055f6170
Luis Caparroz <lcaparroz.dev@gmail.com>
2019-04-07 20:59:23 -0300

    Rename generateRawField to generateField
    This commit renames the Minesweeper function `generateRawField` to
    `generateField` and changes its behavior. Now, the function returns a
    map (immutable Map) where the keys are coordinates (immutable List) and
    the values are nested maps with basic vertice information.

--- app/minesweeper.js
+++ app/minesweeper.js
@@ -1,9 +1,15 @@
-const { List, Range } = require('immutable');
+const { List, Map, Range } = require('immutable');
 
-exports.generateRawField = function(height, width) {
-  return Range(0, height).map(
-    x => Range(0, width).map(
-      y => List([x, y])
-    )
-  );
+exports.generateField = function(height, width) {
+  return Range(0, height)
+    .map(x => Range(0, width).map(y => List([x, y])))
+    .flatten(true)
+    .reduce(
+      (field, vertice) => {
+        return field.set(
+          vertice, Map({ mine: false, marked: false, uncovered: false })
+        )
+      },
+      Map()
+    );
 };
--- test/minesweeper.js
+++ test/minesweeper.js
@@ -1,19 +1,31 @@
 const assert = require('assert');
-const { List, Seq } = require('immutable');
+const { List, Map } = require('immutable');
 const minesweeper = require ("../app/minesweeper");
 
 describe("Minesweeper", function() {
-  describe("generateRawField", function() {
-    it("returns a matrix with elements as their own coordinates", function() {
-      const expected_field = Seq(
-        [
-          Seq([List([0, 0]), List([0, 1]), List([0, 2])]),
-          Seq([List([1, 0]), List([1, 1]), List([1, 2])]),
-          Seq([List([2, 0]), List([2, 1]), List([2, 2])])
-        ]
-      );
+  describe("generateField", function() {
+    it("returns an empty field without mines", function() {
+      const empty_vertice = function() {
+        return Map({
+          mine: false,
+          marked: false,
+          uncovered: false
+        });
+      };
 
-      const result_field = minesweeper.generateRawField(3, 3);
+      const expected_field = Map([
+        [ List([0, 0]), empty_vertice() ],
+        [ List([0, 1]), empty_vertice() ],
+        [ List([0, 2]), empty_vertice() ],
+        [ List([1, 0]), empty_vertice() ],
+        [ List([1, 1]), empty_vertice() ],
+        [ List([1, 2]), empty_vertice() ],
+        [ List([2, 0]), empty_vertice() ],
+        [ List([2, 1]), empty_vertice() ],
+        [ List([2, 2]), empty_vertice() ]
+      ]);
+
+      const result_field = minesweeper.generateField(3, 3);
 
       assert(expected_field.equals(result_field));
     });
