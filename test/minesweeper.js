51285da20d8bdc3462f7a68ed89ee1faa969868d
Luis Caparroz <lcaparroz.dev@gmail.com>
2019-04-07 22:09:23 -0300

    Refactor Minesweeper tests
    This commit refactors the Minesweeper tests using expect assertions
    (from "chai") and fix style (change from snake_case to camelCase).

--- test/minesweeper.js
+++ test/minesweeper.js
@@ -1,33 +1,42 @@
-const assert = require('assert');
-const { List, Map } = require('immutable');
+const chai = require('chai');
+const chaiImmutable = require('chai-immutable');
+const immutable = require('immutable');
 const minesweeper = require ("../app/minesweeper");
 
+chai.use(chaiImmutable);
+const { expect } = chai;
+const { List, Map } = immutable;
+
 describe("Minesweeper", function() {
-  describe("generateField", function() {
+  describe("generateField()", function() {
     it("returns an empty field without mines", function() {
-      const empty_vertice = function() {
-        return Map({
-          mine: false,
-          marked: false,
-          uncovered: false
-        });
-      };
+      const expectedField = h3w3Field();
 
-      const expected_field = Map([
-        [ List([0, 0]), empty_vertice() ],
-        [ List([0, 1]), empty_vertice() ],
-        [ List([0, 2]), empty_vertice() ],
-        [ List([1, 0]), empty_vertice() ],
-        [ List([1, 1]), empty_vertice() ],
-        [ List([1, 2]), empty_vertice() ],
-        [ List([2, 0]), empty_vertice() ],
-        [ List([2, 1]), empty_vertice() ],
-        [ List([2, 2]), empty_vertice() ]
-      ]);
+      const resultField = minesweeper.generateField(3, 3);
 
-      const result_field = minesweeper.generateField(3, 3);
-
-      assert(expected_field.equals(result_field));
+      expect(resultField).to.equal(expectedField);
     });
   });
 });
+
+function h3w3Field() {
+  const emptyVertice = function() {
+    return Map({
+      mine: false,
+      marked: false,
+      uncovered: false
+    });
+  };
+
+  return Map([
+    [ List([0, 0]), emptyVertice() ],
+    [ List([0, 1]), emptyVertice() ],
+    [ List([0, 2]), emptyVertice() ],
+    [ List([1, 0]), emptyVertice() ],
+    [ List([1, 1]), emptyVertice() ],
+    [ List([1, 2]), emptyVertice() ],
+    [ List([2, 0]), emptyVertice() ],
+    [ List([2, 1]), emptyVertice() ],
+    [ List([2, 2]), emptyVertice() ]
+  ]);
+};
