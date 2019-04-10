bf96cf8a92b3a5fc68a5edb0feba194d1757e1f8
Luis Caparroz <lcaparroz.dev@gmail.com>
2019-04-09 23:08:04 -0300

    Rewrite adjacentVertices test dynamically
    This commit rewrites the tests for the Minesweeper function
    `adjacentVertices` to be generated dynamically.

--- test/minesweeper.js
+++ test/minesweeper.js
@@ -19,7 +19,7 @@ describe("Minesweeper", function() {
   });
 
   describe("populateFieldWithMines()", function() {
-    it("returns a field with the given number of mines", function () {
+    it("returns a field with the given number of mines", function() {
       const emptyField = h3w3Field();
 
       const resultField = minesweeper.populateFieldWithMines(emptyField, 5);
@@ -33,129 +33,118 @@ describe("Minesweeper", function() {
   });
 
   describe("adjacentVertices()", function() {
-    context("when the vertice's on the top-left corner", function () {
-      it("returns 3 adjacent vertices", function() {
-        const expectedAdjacent = Set.of(
+    const tests = [
+      {
+        position_text: "top-left corner",
+        x: 0,
+        y: 0,
+        height: 5,
+        width: 5,
+        expected: Set.of(
           List([1, 0]),
           List([0, 1]),
           List([1, 1])
-        );
-
-        const resultAdjacent = minesweeper.adjacentVertices(0, 0, 5, 5);
-
-        expect(resultAdjacent).to.equal(expectedAdjacent)
-      });
-    });
-
-    context("when the vertice's on the top-right corner", function () {
-      it("returns 3 adjacent vertices", function() {
-        const expectedAdjacent = Set.of(
+        )
+      },
+      {
+        position_text: "top-right corner",
+        x: 4,
+        y: 0,
+        height: 5,
+        width: 5,
+        expected: Set.of(
           List([3, 0]),
           List([3, 1]),
           List([4, 1])
-        );
-
-        const resultAdjacent = minesweeper.adjacentVertices(4, 0, 5, 5);
-
-        expect(resultAdjacent).to.equal(expectedAdjacent)
-      });
-    });
-
-    context("when the vertice's on the bottom-left corner", function () {
-      it("returns 3 adjacent vertices", function() {
-        const expectedAdjacent = Set.of(
+        )
+      },
+      {
+        position_text: "bottom-left corner",
+        x: 0,
+        y: 4,
+        height: 5,
+        width: 5,
+        expected: Set.of(
           List([0, 3]),
           List([1, 3]),
           List([1, 4])
-        );
-
-        const resultAdjacent = minesweeper.adjacentVertices(0, 4, 5, 5);
-
-        expect(resultAdjacent).to.equal(expectedAdjacent)
-      });
-    });
-
-    context("when the vertice's on the bottom-right corner", function () {
-      it("returns 3 adjacent vertices", function() {
-        const expectedAdjacent = Set.of(
+        )
+      },
+      {
+        position_text: "bottom-right corner",
+        x: 4,
+        y: 4,
+        height: 5,
+        width: 5,
+        expected: Set.of(
           List([3, 3]),
           List([4, 3]),
           List([3, 4])
-        );
-
-        const resultAdjacent = minesweeper.adjacentVertices(4, 4, 5, 5);
-
-        expect(resultAdjacent).to.equal(expectedAdjacent)
-      });
-    });
-
-    context("when the vertice's in the middle of the top side", function () {
-      it("returns 5 adjacent vertices", function() {
-        const expectedAdjacent = Set.of(
+        )
+      },
+      {
+        position_text: "middle of the top side",
+        x: 2,
+        y: 0,
+        height: 5,
+        width: 5,
+        expected: Set.of(
           List([1, 0]),
           List([3, 0]),
           List([1, 1]),
           List([2, 1]),
           List([3, 1])
-        );
-
-        const resultAdjacent = minesweeper.adjacentVertices(2, 0, 5, 5);
-
-        expect(resultAdjacent).to.equal(expectedAdjacent)
-      });
-    });
-
-    context("when the vertice's in the middle of the bottom side", function () {
-      it("returns 5 adjacent vertices", function() {
-        const expectedAdjacent = Set.of(
+        )
+      },
+      {
+        position_text: "middle of the bottom side",
+        x: 2,
+        y: 4,
+        height: 5,
+        width: 5,
+        expected: Set.of(
           List([1, 3]),
           List([2, 3]),
           List([3, 3]),
           List([1, 4]),
           List([3, 4])
-        );
-
-        const resultAdjacent = minesweeper.adjacentVertices(2, 4, 5, 5);
-
-        expect(resultAdjacent).to.equal(expectedAdjacent)
-      });
-    });
-
-    context("when the vertice's in the middle of the left side", function () {
-      it("returns 5 adjacent vertices", function() {
-        const expectedAdjacent = Set.of(
+        )
+      },
+      {
+        position_text: "middle of the left side",
+        x: 0,
+        y: 2,
+        height: 5,
+        width: 5,
+        expected: Set.of(
           List([0, 1]),
           List([1, 1]),
           List([1, 2]),
           List([0, 3]),
           List([1, 3])
-        );
-
-        const resultAdjacent = minesweeper.adjacentVertices(0, 2, 5, 5);
-
-        expect(resultAdjacent).to.equal(expectedAdjacent)
-      });
-    });
-
-    context("when the vertice's in the middle of the right side", function () {
-      it("returns 5 adjacent vertices", function() {
-        const expectedAdjacent = Set.of(
+        )
+      },
+      {
+        position_text: "middle of the right side",
+        x: 4,
+        y: 2,
+        height: 5,
+        width: 5,
+        expected: Set.of(
           List([3, 1]),
           List([4, 1]),
           List([3, 2]),
           List([3, 3]),
           List([4, 3])
-        );
-
-        const resultAdjacent = minesweeper.adjacentVertices(4, 2, 5, 5);
-
-        expect(resultAdjacent).to.equal(expectedAdjacent)
-      });
-    });
-
-    context("when the vertice's in the middle of the field", function () {
-      it("returns 8 adjacent vertices", function() {
-        const expectedAdjacent = Set.of(
+        )
+      },
+      {
+        position_text: "middle of the field",
+        x: 2,
+        y: 2,
+        height: 5,
+        width: 5,
+        expected: Set.of(
           List([1, 1]),
           List([2, 1]),
           List([3, 1]),
@@ -164,11 +153,18 @@ describe("Minesweeper", function() {
           List([1, 3]),
           List([2, 3]),
           List([3, 3])
-        );
+        )
+      }
+    ];
 
-        const resultAdjacent = minesweeper.adjacentVertices(2, 2, 5, 5);
+    tests.forEach(function(test) {
+      context("when the vertice's in the " + test.position_text, function() {
+        it("returns " + test.expected.size + " adjacent vertices", function() {
+          const resultAdjacent = minesweeper
+            .adjacentVertices(test.x, test.y, test.height, test.width);
 
-        expect(resultAdjacent).to.equal(expectedAdjacent)
+          expect(resultAdjacent).to.equal(test.expected)
+        });
       });
     });
   });
